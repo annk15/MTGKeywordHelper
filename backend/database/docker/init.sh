cec190e19094d1b9768beb72f8c288473f8ab7ea
Anton Nilsson <antonnilsson@Antons-MacBook-Pro.local>
2024-07-26 20:40:49 +0200

    4 - Update scripts

--- backend/database/docker/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mysql --protocol=tcp -u root -ppassword -P 3307 < ./init.sql
-
--- backend/database/docker/init.sql
+++
@@ -1,3 +0,0 @@
-create database mtg;
-use mtg;
-create table keyword_abilities(Keyword varchar(50), Description varchar(500));
