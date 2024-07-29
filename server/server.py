0fa5b1c105e940477b628e077ed7b7471eded5cb
annk15 <36030917+annk15@users.noreply.github.com>
2024-07-29 23:34:14 +0200

    Add new endpoint for keywords (#38)
    Co-authored-by: Anton Nilsson <antonnilsson@Antons-MacBook-Pro.local>

--- server/server.py
+++ server/server.py
@@ -7,7 +7,6 @@ from io import BytesIO
 app = Flask(__name__)
 CORS(app)
 
-# Database configuration
 DB_CONFIG = {
   "host": "127.0.0.1",
   "port": 3307,
@@ -17,39 +16,69 @@ DB_CONFIG = {
 }
 
 def getConnection():
+
     try:
+
         connection = mysql.connector.connect(**DB_CONFIG)
+
         if connection.is_connected():
             return connection
+
     except Error as e:
         print(f"Error connecting to MySQL: {e}")
+
     return None
 
 def fetchImageBinary(keyword):
+
     connection = getConnection()
     cursor = connection.cursor()
+
     sql = "SELECT keyword, image FROM keyword_images WHERE keyword = %s"
+
     cursor.execute(sql, (keyword,))
     result = cursor.fetchone()
+
     cursor.close()
     connection.close()
+
     if result:
         return BytesIO(result[1])
     return None
 
 @app.route('/keyword-abilities', methods=['GET'])
 def keywordAbilities():
+
     connection = getConnection()
     cursor = connection.cursor(dictionary=True)
+
     cursor.execute("SELECT * FROM keyword_abilities")
     items = cursor.fetchall()
+
     cursor.close()
     connection.close()
+
     return jsonify(items), 200
+
+@app.route('/keywords', methods=['GET'])
+def keywords():
+
+    connection = getConnection()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT keyword FROM keyword_abilities")
+    items = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return jsonify([item['keyword'] for item in items]), 200
 
 @app.route('/keyword-image/<string:keyword>', methods=['GET'])
 def keywordImages(keyword):
+
     image = fetchImageBinary(keyword)
+
     if image:
         return send_file(image, mimetype='image/jpeg')
     else:
