7cec9c82b97324b6a3405fbe76e3c4c2d558c5e1
annk15 <36030917+annk15@users.noreply.github.com>
2024-08-03 19:48:12 +0200

    Autocompletion + new interface code structure (#45)
    * Add function for getting image url and adding image to cards
    
    * Add comments and reformat
    
    * Restructure card div and style
    
    * Add config.js. Change fetchImage() name. Add release image url after loaded in createCare().
    
    * Add card animation
    
    * Css changes
    
    * Add mobile style change. Add additional files to gitignore.
    
    * Add vanilla-tilt.js library
    
    * Add gsap
    
    * Fix merge issue
    
    * Restructure site directory and add site server
    
    * Add clean scripts to interface folder
    
    * Card design fixes
    
    * Change card style
    
    * Revert merge
    
    * Add shadow to card
    
    * WIP
    
    * Update search logic to only trigger on Enter
    
    * Add back-to-top button
    
    * Add gitignore item
    
    ---------
    
    Co-authored-by: git-hub-oscar <118494885+git-hub-oscar@users.noreply.github.com>
    Co-authored-by: Anton Nilsson <antonnilsson@Antons-MacBook-Pro.local>

--- interface/static/js/generate-card.js
+++ interface/static/js/generate-card.js
@@ -1,67 +1,25 @@
-import config from './config.js';
+import KeywordsNotifier from './data-notifier.js';
+import DataHandler from './data-handler.js'
 
-// Listen for DOM content being fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-    const cardHolder = document.getElementById('cardHolder');
+export default class CardContainer {
 
-    // Fetch card data from API
-    async function fetchCardData() {
-        const url = `${config.apiUrl}${config.endpoint.keyword}`;
-        console.log(url);
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Error : Endpoint (Keyword abilities) responded with: ${response.status}`);
-            }
-            return await response.json();
-        }
-        catch (error) {
-            console.error('Error fetching cards data:', error.message);
-        }
+    constructor(cardHolder) {
+        this.cardHolder = cardHolder;
+        KeywordsNotifier.regKeywordInfoData(this);
     }
 
-    // Fetches the Image
-    async function fetchImage(keyword) {
-        const url = `${config.apiUrl}${config.endpoint.keywordImage(keyword)}`;  // Use the keyword in the URL
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Error fetching image for keyword '${keyword}': ${response.status}`);
-            }
-            const blob = await response.blob();
-            const imageUrl = URL.createObjectURL(blob);
-            return imageUrl;
-        } catch (error) {
-            console.error('Error fetching image URL:', error.message);
-        }
-    }
-
-    // Creates a card element with a title, image and description
-    async function createCard(keyword, description) {
-        const imageUrl = await fetchImage(keyword); //Fetches the imageurl related to the keyword property
-
+    // Create a card
+    createCard(keyword, description) {
         // Create a new div element for the card and adds 'card' and 'visible' class
         const card = document.createElement('div');
         card.classList.add('card');
-        
+
         // Creates a image and adds classes
         const cardImage = document.createElement('div');
         cardImage.classList.add('cardImage');
-        cardImage.style.backgroundImage = `url(${imageUrl})`;
 
         const cardTextContainer = document.createElement('div'); // Create div for text container
         cardTextContainer.classList.add('cardTextContainer'); // Give it the class 'cardTextContainer'
-        
-        //potential filter. Needs to fix the issue of text behind filter. Need to see if images are being fetched twice per image.
-        //cardTextContainer.style.backgroundImage = `url(${imageUrl})`;
-        //cardTextContainer.style.filter = 'blur(10px) brightness(0.2)';
-        //cardTextContainer.style.transform = "rotate(180deg";
-        //cardTextContainer.style.backgroundPosition = "bottom";
-        
-        //Release the object URL after it's loaded
-        cardImage.addEventListener('load', () => {
-            URL.revokeObjectURL(imageUrl);
-        });
 
         const cardTitle = document.createElement('h2'); // Create div for title
         cardTitle.classList.add('cardTitle'); // Give it the class 'cardTitle'
@@ -76,35 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
         card.appendChild(cardTextContainer)
         cardTextContainer.appendChild(cardTitle);
         cardTextContainer.appendChild(cardDescription);
-        
+
+        // Fetch card image
+        DataHandler.fetchKeywordImageData(data => {
+            cardImage.style.backgroundImage = `url(${data})`;
+            cardImage.addEventListener('load', () => {
+                URL.revokeObjectURL(data);
+            });
+        }, keyword)
+
         return card; // Return the constructed card element
     }
 
-    // Uses fetchCardData() and fetchImage to get the data from API and createCard() to make a card for each object, then renders them on the page.
-    // Uses fetchCardData() and fetchImage to get the data from API and createCard() to make a card for each object, then renders them on the page.
-    async function renderCards() {
-        const cardData = await fetchCardData();
-
-        // Creates a card for each object in the fetched data and append to cardHolder
-        cardData.forEach(async ({ keyword, description }) => {
+    // Add each card to the cardholder when new data is recived
+    onKeywordInfoData(data) {
+        data.forEach(async ({ keyword, description }) => {
             try {
-                const card = await createCard(keyword, description);
+                const card = this.createCard(keyword, description);
                 cardHolder.appendChild(card);
             } catch (error) {
                 console.error('Error rendering cards:', error.message);
             }
         });
     }
-
-
-    // Initialize card rendering
-    async function initialize() {
-        try {
-            await renderCards();
-        } catch (error) {
-            console.error('Error in Initialize:', error.message);
-        }
-    }
-
-    initialize();
-});
+}
--- interface/static/js/search.js
+++ interface/static/js/search.js
@@ -1,72 +1,19 @@
-const searchBar = document.getElementById('keywordSearch');
+import KeywordsNotifier from './data-notifier.js';
 
-// Debounce function to limit the rate at which the search function executes
-function debounce(func, delay) {
-    let debounceTimer;
-    return function () {
-        const context = this;
-        const args = arguments;
-        clearTimeout(debounceTimer);
-        debounceTimer = setTimeout(() => func.apply(context, args), delay);
-    };
-}
+export default class Search {
 
-//Logic allowing slight discrepancies between user input and card title name, making searches less strict for finding cards
-function levenshtein(a, b) {
-    const an = a.length; // Searchbar input
-    const bn = b.length; //Keyword card titles
+  constructor() {
+    KeywordsNotifier.regKeywordInfoData(this);
+  }
 
-    //This is the early exit condition, immedietly returning the distance incase one value is empty to prevent further processing
-    if (an === 0) return bn;  // If 'a' is empty, return length of 'b'
-    if (bn === 0) return an; // If 'b' is empty, return length of 'a'
-
-    const matrix = [];
-
-    // Initialize matrix with base cases
-    for (let i = 0; i <= bn; i++) {
-        matrix[i] = [i];
-    }
-
-    for (let j = 0; j <= an; j++) {
-        matrix[0][j] = j;
-    }
-    // Populate the matrix with Levenshtein distances
-    for (let i = 1; i <= bn; i++) {
-        for (let j = 1; j <= an; j++) {
-            if (b.charAt(i - 1) === a.charAt(j - 1)) {
-                matrix[i][j] = matrix[i - 1][j - 1]; // No change needed if characters match
-            } else {
-                // Calculate the minimum edit distance considering substitution, insertion, and deletion
-                matrix[i][j] = Math.min(
-                    matrix[i - 1][j - 1] + 1, // substitution
-                    matrix[i][j - 1] + 1, // insertion
-                    matrix[i - 1][j] + 1 // deletion
-                );
-            }
-        }
-    }
-
-    return matrix[bn][an]; // Return the computed distance
-}
-
-//Function for searching for keyword titles among cards
-function performSearch(e) {
-    let input = e.target.value.toLowerCase().trim();
-    let keywordTitles = document.querySelectorAll('h2.cardTitle');
-    
-    const maxDistance = 1; // Maximum allowed Levenshtein distance for fuzzy matching
-    keywordTitles.forEach(title => {
+    // Show or hide cards, all cards will be shown if parameter is undefined
+    showCards(matchingTags) {
+      let keywordTitles = document.querySelectorAll('h2.cardTitle');
+      keywordTitles.forEach(title => {
         let grandParentCard = title.parentNode.parentNode;
-        let titleText = title.textContent.toLowerCase().trim();
+        let titleText = title.textContent.trim();
 
-        // Check if the input is a substring of the titleText
-        let isSubstringMatch = titleText.includes(input);
-
-        // Check Levenshtein distance if not a substring match
-        let isFuzzyMatch = !isSubstringMatch && levenshtein(input, titleText) <= maxDistance;
-
-        // Show or hide the card based on matching criteria
-        if (isSubstringMatch || isFuzzyMatch) {
+        if(matchingTags == undefined || matchingTags.includes(titleText)) {
             if(grandParentCard.classList.contains('hidden')) {
                 grandParentCard.classList.add('visible');
                 grandParentCard.classList.remove('hidden');
@@ -75,8 +22,36 @@ function performSearch(e) {
             grandParentCard.classList.add('hidden');
             grandParentCard.classList.remove('visible');
         }
-    });
-}
+      });
+    }
 
-//Runs performSearch after a debounce delay, in favor of performance
-searchBar.addEventListener('keyup', debounce(performSearch, 300));
+    onKeywordInfoData(data) {
+      var availableTags = []
+
+      // Extract keyword data from json
+      data.forEach(({ keyword }) => {
+        availableTags.push(keyword);
+      });
+
+      // Create a bind to access outer function
+      const showCardsBind = this.showCards.bind(this);
+
+      // Customize autocomplete list to only show 5 items
+      $("#search").autocomplete({
+        source: function(request, response) {
+          var results = $.ui.autocomplete.filter(availableTags, request.term);
+          response(results.slice(0, 10));
+        }
+      });
+
+      // Filter cards on Enter
+      $("#search").on("keydown", function(event) {
+        if (event.key === "Enter") {
+          console.info("ENTER!")
+            var results = $.ui.autocomplete.filter(availableTags, $(this).val());
+            showCardsBind(results)
+        }
+    });
+
+    }
+}
