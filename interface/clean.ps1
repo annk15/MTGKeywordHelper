4506bc7294ca88a126007d07c38fb293c72abd8e
git-hub-oscar <118494885+git-hub-oscar@users.noreply.github.com>
2024-08-23 23:56:05 +0200

    Remove interface folder in preperation for react

--- interface/clean.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "Will perform a clean on the interface"
-
-Remove-Item -Recurse -Force venv
-if ($LASTEXITCODE -ne 0) {
-    Write-Output "Failed to remove the python environment"
-    exit 1
-}
-
-Write-Output "Done, Artifacts were successfully removed!"
